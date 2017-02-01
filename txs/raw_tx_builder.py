ba66c8b3403248ff6a62edefbace57d77dc32a00
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2017-02-01 16:57:40 +0100

    Modifies raw_tx_builder to allow including other type of ScriptSig, or no one, in case a multisig is desired to be included afterwards.

--- txs/raw_tx_builder.py
+++ txs/raw_tx_builder.py
@@ -3,24 +3,32 @@ from tx import TX
 from wallet.wallet import generate_std_scriptpubkey, get_priv_key_hex
 
 
-def build_raw_tx(prev_tx_id, prev_out_index, src_btc_addr, value, dest_btc_addr):
+def build_raw_tx(prev_tx_id, prev_out_index, src_btc_addr, value, dest_btc_addr, scriptPubKey=None, scriptSig=None):
 
     assert len(prev_tx_id) == len(prev_out_index) == len(src_btc_addr)
     assert len(value) == len(dest_btc_addr)
 
-    scriptPubKey = []
-    for i in range(len(dest_btc_addr)):
-        scriptPubKey.append(generate_std_scriptpubkey(dest_btc_addr[i]))
+    if scriptPubKey is None:
+        scriptPubKey = []
+        for i in range(len(dest_btc_addr)):
+            scriptPubKey.append(generate_std_scriptpubkey(dest_btc_addr[i]))
+    else:
+        assert len(scriptPubKey) == len(dest_btc_addr)
 
     tx = TX()
-    tx.build_p2pkh_std_tx(prev_tx_id, prev_out_index, value, scriptPubKey)
+    raw_tx = ""
 
-    signed_tx = ""
-    for i in range(len(src_btc_addr)):
-        priv_key = src_btc_addr[i] + "/sk.pem"
-        priv_key_hex = get_priv_key_hex(priv_key)
-        signed_tx = sign(tx.hex, i, priv_key_hex)
+    if scriptSig is None:
+        tx.build_p2pkh_std_tx(prev_tx_id, prev_out_index, value, scriptPubKey)
+        for i in range(len(src_btc_addr)):
+            priv_key = src_btc_addr[i] + "/sk.pem"
+            priv_key_hex = get_priv_key_hex(priv_key)
+            raw_tx = sign(tx.hex, i, priv_key_hex)
 
-    tx.print_elements()
-    return signed_tx
+    else:
+        assert len(scriptPubKey) == len(src_btc_addr)
+        tx.build_p2pkh_std_tx(prev_tx_id, prev_out_index, value, scriptPubKey, scriptSig, scriptSig)
+        raw_tx = tx.hex
+
+    return raw_tx
 
