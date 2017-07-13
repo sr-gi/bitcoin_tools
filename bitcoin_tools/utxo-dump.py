c8f90cef6b9e7191363a144be23e203085e1a3a4
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2017-07-13 11:56:06 +0200

    Updates dump with config file.

--- bitcoin_tools/utxo-dump.py
+++ bitcoin_tools/utxo-dump.py
@@ -1,9 +1,16 @@
 from bitcoin_tools.utils import decode_utxo, change_endianness
 from json import loads, dumps
+try:
+    import bitcoin_tools.conf as cfg
+except ImportError:
+    raise Exception("You don't have a configuration file. Make a copy of sample_conf.py")
+
+if cfg.btc_core_path is None or cfg.data_path is None:
+    raise Exception("Your configuration file is not properly configured.")
 
 # Input and output files
-fin = open('./utxos.txt', 'r')
-fout = open('./parsed_txs.txt', 'w')
+fin = open(cfg.data_path + 'utxos.txt', 'r')
+fout = open(cfg.data_path + 'parsed_txs.txt', 'w')
 
 types = [0, 1, 2, 3, 4, 5]
 for line in fin:
