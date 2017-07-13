affe6d8cb333a22f4dd8585e737966d7f53175c6
cpsola <cperez@deic.uab.cat>
2017-07-13 17:51:49 +0200

    dump transaction and utxo data

--- bitcoin_tools/utxo-dump.py
+++ bitcoin_tools/utxo-dump.py
@@ -4,17 +4,43 @@ from json import loads, dumps
 # Load config file
 cfg = load_conf_file()
 
-# Input and output files
+# Input file
 fin = open(cfg.data_path + 'utxos.txt', 'r')
-fout = open(cfg.data_path + 'parsed_txs.txt', 'w')
 
-types = [0, 1, 2, 3, 4, 5]
+# Transaction dump
+fout_tx = open(cfg.data_path + 'parsed_txs.txt', 'w')
+for line in fin:
+    data = loads(line[:-1])
+    utxo = decode_utxo(data["value"])
+
+    imprt = sum([out["amount"] for out in utxo.get("outs")])
+
+    result = {"tx_id": change_endianness(data["key"][2:]),
+              "num_utxos": len(utxo.get("outs")),
+              "total_value": imprt,
+              "total_len": (len(data["key"]) + len(data["value"]))/2,
+              "height": utxo["height"],
+              "coinbase": utxo["coinbase"],
+              "version": utxo["version"]}
+
+    fout_tx.write(dumps(result) + '\n')
+
+fout_tx.close()
+
+# UTXO dump
+fout_utxo = open(cfg.data_path + 'parsed_utxo.txt', 'w')
 for line in fin:
     data = loads(line[:-1])
     utxo = decode_utxo(data["value"])
 
     for out in utxo.get("outs"):
-        result = {"tx_id": change_endianness(data["key"][2:])}
-        result.update(out)
-        fout.write(dumps(result) + '\n')
 
+        result = {"tx_id": change_endianness(data["key"][2:]),
+                  "tx_height": utxo["height"],
+                  "utxo_data_len": len(out["data"])/2}
+
+        result.update(out)
+
+        fout_utxo.write(dumps(result) + '\n')
+
+fout_utxo.close()
