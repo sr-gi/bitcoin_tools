b0365b7ed238ee57d0720cdcea2a3c435f413cc9
Sergi Delgado <sergi.delgado.s@gmail.com>
2018-03-12 15:25:38 +0100

    Creates get compressed_pk and get_uncompressed_pk fuctions to convert between both pk representation types. #close #8

--- bitcoin_tools/core/keys.py
+++ bitcoin_tools/core/keys.py
@@ -145,3 +145,55 @@ def ecdsa_tx_sign(unsigned_tx, sk, hashflag=SIGHASH_ALL, deterministic=True):
 
     # Finally, add the hashtype to the end of the signature as a 2-byte big endian hex value.
     return hexlify(s) + hc[-2:]
+
+
+def get_compressed_pk(pk):
+    """
+    Constructs the compressed representation of a SECP256k1 ECDSA public key form a given uncompressed key.
+
+    :param pk: The uncompressed SECP256k1 key to be decompressed.
+    :type pk: hex
+    :return: The compressed SECP256k1 ECDSA key.
+    :rtype: hex
+    """
+
+    ecdsa_pk = VerifyingKey.from_string(unhexlify(pk[2:]), curve=SECP256k1)
+    compressed_pk = serialize_pk(ecdsa_pk)
+
+    return compressed_pk
+
+
+def get_uncompressed_pk(compressed_pk):
+    """
+    Constructs the uncompressed representation of a SECP256k1 ECDSA public key form a given compressed key.
+
+    The code is port from https://stackoverflow.com/a/43654055/5413535 with a couple of bug fixed.
+
+    :param compressed_pk: The compressed SECP256k1 key to be decompressed.
+    :type compressed_pk: hex
+    :return: The uncompressed SECP256k1 ECDSA key.
+    :rtype: hex
+    """
+
+    # Get p from the curve
+    p = SECP256k1.curve.p()
+
+    # Get x and the prefix
+    x_hex = compressed_pk[2:66]
+    x = int(x_hex, 16)
+    prefix = compressed_pk[0:2]
+
+    # Compute y
+    y_square = (pow(x, 3, p) + 7) % p
+    y_square_square_root = pow(y_square, (p + 1) / 4, p)
+
+    # Chose the proper y depending on the prefix and whether the computed square root is odd or even
+    if prefix == "02" and y_square_square_root & 1 or prefix == "03" and not y_square_square_root & 1:
+        y = (-y_square_square_root) % p
+    else:
+        y = y_square_square_root
+
+    # Construct the uncompressed pk
+    uncompressed_pk = "04" + x_hex + format(y, '064x')
+
+    return uncompressed_pk
